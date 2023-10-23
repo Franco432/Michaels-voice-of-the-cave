cecd19780f2895827bb710de78caf9852dac0472
Franco <frankegamer59@gmail.com>
2023-10-22 22:51:14 -0300

    Añadir música entre escenas

--- sources/escenas.py
+++ sources/escenas.py
@@ -6,6 +6,16 @@ from pygame import QUIT, MOUSEBUTTONUP
 from pygame.time import get_ticks
 from pygame.display import flip
 from pygame.event import get
+from pygame.mixer import music
 from threading import Thread
 
-path = "/Users/frank/Desktop/Michaels-voice-of-the-cave/assets/"
+path = "/Michaels-voice-of-the-cave/assets/"
+path_juego = path + 'game/'
+path_menu  = path + 'menu/'
+path_pausa = path + 'pausa/'
+
+# Create function to change and play music in cualquier scene
+def musicar(cancion:str, segundo:float=0):
+	music.load(cancion)
+	music.play(-1)
+	music.set_pos(segundo)
